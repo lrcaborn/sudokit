0a4b1d997aee912838377c444757bee47b9200e2
lrcaborn <github.com@lrcaborn.33mail.com>
2015-04-30 20:29:11 -0400

    Added test project, and cell class.

---
+++ Test/Unit/CellTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using lrc.sudokit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lrc.sudokit
+{
+    [TestClass]
+    public class CellTest
+    {
+        [TestMethod]
+        [Owner("LRC")]
+        public void CanInstantiate()
+        {
+            var cell = new Cell();
+            Assert.IsInstanceOfType(cell, typeof(Cell));
+        }
+
+        [TestMethod]
+        [Owner("LRC")]
+        public void ValuesEmptyWhenInstantiated()
+        {
+            var cell = new Cell();
+            Assert.IsTrue(cell.Values.Length == 9);
+        }
+
+    }
+}
---
+++ sudokit/Application/Cell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lrc.sudokit
+{
+    public class Cell
+    {
+        public Byte[] Values;
+
+        public Cell(){
+            Values = new Byte[] {1,2,3,4,5,6,7,8,9};
+    }
+    
+    }
+}
